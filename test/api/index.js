1c02500c11ac1fc57801199dfe194a36d87c1aaf
GZolla <43836485+GZolla@users.noreply.github.com>
2022-08-20 16:42:08 -0700

    Refactor existing tests to new version of testing API

--- test/api/index.js
+++ test/api/index.js
@@ -1,158 +1,43 @@
 // @ts-check
 import {API} from "../../addOns/API.js"
-import Test from "../../addOns/Test.js"
+import {Test,ClassTest} from "../../testing/index.js"
 
-/* ----------------------------------------------------------------------------------------------------
-Imported Types: Change if support ever
----------------------------------------------------------------------------------------------------- */
-
-
-/**
- * @callback ObjectCallback Callback that takes an object as only parameter
- * @param {Object.<string,*>} param
- */
-
-/**
- * @typedef {Object} LoadingData
- * @property {ObjectCallback} renderer
- * @property {Object.<string,*>} render_param
- * @property {ObjectCallback} derenderer
- * @property {Object.<string,*>} derender_param
- */
-
-/**
- * @typedef {('POST'|'PUT'|'DELETE'|'GET')} RESTMethod
- */
-
-/**
- * @typedef {Object} RequestBody
- * @property {RESTMethod} method
- * @property {string} body
- * @property {RequestHeader} headers
- * @property {RequestCredentials} credentials
- */
-
-/**
- * @typedef {Object} RequestHeader
- * @property {string} `Content-Type`
- * @property {string} `X-CSRFToken` 
- */
-
-/**
- * @callback ResponseCallback Callback that takes a promise of an API request
- * @param {Promise.<Object.<string,string> | {}>} promise
- */
-
-// Test.js
-
-/**
- * @template {*[]} Input Type of input parameters
- * @template {*} Actual Type of actual output
- * @template {*} Expected Type of expected output
- * @callback CheckFunctionCall Check actual result against expected result
- * @param {Actual} actual The actual result of the function call
- * @param {Expected} expected The expected result of the function call
- * @param {Input} input The input parameters passed to the function call
- * @param {HTMLElement} [parent] The parent element in which to display errors
- * @param {number} [index] Index of the check being applied
- * @return {(string | null) | Promise<(string | null)>}  Either null or error message on fail
- */
-
-
-/**
- * @template {any[]} Input Type of input parameters
- * @template {any} Expected Type of expected output
- * @typedef {Object} Check An input, an expected output and wether the output is an exception
- * @property {Input} input The input parameters passed to the function call
- * @property {Expected} expected The expected result of the function call
- * @property {boolean} expectException Expect callback to throw exception
- */
-
-/**
- * @template {any[]} Input Type of input parameters
- * @template {any} Expected Type of expected output
- * @template {any} ExceptionExpected Type of expected output on exception
- * @typedef {Check<Input,Expected> | Check<Input,ExceptionExpected>} CheckUnion A union of Checks with same input type but different expected types for exceptions
- */
-
-/**
- * @template {*[]} Input Type of input parameters
- * @template {*} Actual Type of actual output
- * @template {*} Expected Type of expected output
- * @template {*} ExceptionExpected Type of expected output on exception
- * @typedef {Object} InstanceFunctionTest A log of a class function to test against given checks
- * @property {string} name Name of the class function
- * @property {CheckUnion<Input,Expected,ExceptionExpected>[]} checks Checks to perform on the function
- * @property {CheckFunctionCall<Input,Actual,Expected>} checkFunction Function to evaluate checks
- * @property {CheckFunctionCall<Input,Actual,ExceptionExpected>} [throwCheckFunction] Function to evaluate checks on an Exception
- * @property {boolean} [parallelize] Wether to run the tests in parallel as opposed to sequentially. WARNING: async functions await per iteration if processed sequentially.
- */
-
-/**
- * @template {Array<*>} Input Type of input parameters
- * @template {*} Actual Type of actual output
- * @template {*} Expected Type of expected output
- * @template {*} ExceptionExpected Type of expected output on exception
- * @typedef {Object} ClassFunctionTest A log of a class function to test against given checks
- * @property {string} name Name of the class function
- * @property {CheckFunctionCall<Input,Actual,Expected>} checkFunction Function to evaluate checks
- * @property {Array<Input>} inputs Inputs for the checks
- * @property {Array<Array<Expected>>} expecteds Expecteds per instance created for the checks
- * @property {Array<Array<ExceptionExpected>>} [exceptionExpecteds] Expecteds per instance created for the checks expecting throws
- * @property {CheckFunctionCall<Input,Actual,ExceptionExpected>} [throwCheckFunction] Function to evaluate checks on an Exception
- * @property {boolean} [parallelize] Wether to run the tests in parallel as opposed to sequentially. WARNING: async functions await per iteration if processed sequentially.
- */
-
-
-/* ----------------------------------------------------------------------------------------------------
-Tests
----------------------------------------------------------------------------------------------------- */
-
-/**
- * @typedef {[url: string, method: RESTMethod, loading_data: LoadingData | null, id?: string | undefined]} APIInput
- */
-/**@type {APIInput[]} */
-const APIInputs = [
-    ["https://pokeapi.co/api/v2/pokemon","GET",null,"pokeApi"],
-    ["https://pokeapi.co/api/v2/pokemon","GET",null,"pokeApi"],
-    ["https://pokeapi.co/api/v2/pokemon","GET",null,undefined],
-    ["https://notarealpage.com","GET",null,""]
-]
-const APIExpecteds = [
-    {id:"pokeApi"},
-    {id:"pokeApi(0)"},
-    {id:`${APIInputs[2][0]}:GET`},
-    {id:``}
-]
-
-
-/**@type {Array<ClassFunctionTest<*[],*,*,*>>} */
-const APIFunctionTests = [
-    {
-        name:"sendRequest",
-        inputs:[[{limit:1}]],
-        expecteds: [
-            [{"count":1154,"next":"https://pokeapi.co/api/v2/pokemon?offset=1&limit=1","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"}]}],
-            [{"count":1154,"next":"https://pokeapi.co/api/v2/pokemon?offset=1&limit=1","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"}]}],
-            [{"count":1154,"next":"https://pokeapi.co/api/v2/pokemon?offset=1&limit=1","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"}]}],
-            [{"error":new Error()}]
-        ],
-        checkFunction:Test.isDeepEqual
-    }
-]
-
-
-
-Test.class(
-    API,
-    Test.buildMultipleChecks(APIInputs,APIExpecteds),
+const APITest = new ClassTest(API,true);
+await APITest.runTests(
+    [
+        {id:"",input:["https://pokeapi.co/api/v2/pokemon","GET",null,"pokeApi"],expected:{id:"pokeApi"}},
+        {id:"",input:["https://pokeapi.co/api/v2/pokemon","GET",null,"pokeApi"],expected:{id:"pokeApi(0)"}},
+        {id:"",input:["https://pokeapi.co/api/v2/pokemon","GET",null,"pokeApi"],expected:{id:"pokeApi(1)"}},
+        {id:"",input:["https://pokeapi.co/api/v2/pokemon","GET",null,undefined],expected:{id:`https://pokeapi.co/api/v2/pokemon:GET`}},
+    ],
     (actual, expected) => {
-        const sameId = Test.isEqual(actual.id,expected.id);
-        if(sameId) return sameId
-        
-        return null
-    },
-    undefined,
-    APIFunctionTests
+        return Test.isEqual(actual.id,expected.id);
+    }
+)
+APITest.displayLastCases();
+
+API.removeById("pokeApi")
+await APITest.runTests(
+    [
+        {id:"",input:["https://pokeapi.co/api/v2/pokemon","GET",null,"pokeApi"],expected:{id:"pokeApi"}},
+        {id:"",input:["https://notarealpage.com","GET",null,""],expected:{id:``}}
+    ],
+    (actual, expected) => {
+        return Test.isEqual(actual.id,expected.id);
+    }
 )
 
+
+await APITest.runInstanceTests("sendRequest",[
+    {
+        input:[{limit:1}],
+        expecteds: [
+            {"count":1154,"next":"https://pokeapi.co/api/v2/pokemon?offset=1&limit=1","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"}]},
+            {"error":new Error()}
+        ]
+    }
+],Test.isDeepEqual)
+
+APITest.displayLastCases();
+
+console.log(APITest.getParamNames());
--- test/string/index.js
+++ test/string/index.js
@@ -1,67 +1,51 @@
 // @ts-check
 
+import { Carrousel } from "../../addOns/displays/displays.js";
 import StringUtils from "../../base/string.js"
-import Test from "../../addOns/Test.js"
+import {Test} from "../../addOns/testing/testing.js"
 
-Test.function(
-    StringUtils.fillString, 
-    Test.buildMultipleChecks(
-        /** @type {[string,number,string][]} */([ // Inputs
-            ["a",1,"b"],
-            ["b",2," "],
-            ["cd",1,"-"],
-            ["efg",10,"_"]
-        ]),
-        [ // Expecteds
-            "a",
-            " b",
-            "cd",
-            "_".repeat(7) + "efg"
-        ]
-    ),
+const carrousel = new Carrousel();
+
+const fillStringTest = new Test(StringUtils.fillString,true);
+fillStringTest.runTests(
+    [ // Inputs
+        {input:["b",2," "],expected:" b",id:"normal"},
+        {input:["a",1,"b"],expected:"a",id:"filled"},
+        {input:["cd",1,"-"],expected:"cd",id:"overfilled"},
+        {input:["efg",10,"_"],expected:"_".repeat(7) + "efg",id:"many"},
+    ],
     Test.isEqual
-)
+).then((v)=>{
+    carrousel.add(fillStringTest.displayLastCases());
+})
 
 
-/**
- * @callback StringModifier
- * @param {string} string Modifying string
- * @returns {string}
- */
 
 
-Test.function(
-    StringUtils.mapWords, 
-    Test.buildMultipleChecks(
-        /** @type {[string, StringModifier,string?,string?][]} */ ([ // Inputs
-            ["a abc abcd",(s)=>s.length + "",undefined,"-"],
-            ["a_b_c",(s)=>s+"!", "_"],
-            ["!b!c",(s)=>"a","!"]
-        ]),
-        [ // Expecteds
-            "1-3-4",
-            "a! b! c!",
-            "a a a"
-        ]
-    ),
+
+
+const mapWordsTest = new Test(StringUtils.mapWords,true);
+mapWordsTest.runTests(
+    [
+        {input:["a abc abcd",(s)=>s.length + "",undefined,"-"],expected:"1-3-4"},
+        {input:["a_b_c",(s)=>s+"!", "_"],expected:"a! b! c!"},
+        {input:["!b!c",(s)=>"a","!"],expected:"a a a"}
+    ],
     Test.isEqual
-)
+).then((v)=>{
+    carrousel.add(mapWordsTest.displayLastCases())
+})
 
-Test.function(
-    StringUtils.wordToTitleCase, 
-    Test.buildMultipleChecks(
-        /** @type {[string][]} */([ // Inputs
-            [""],
-            ["abc"],
-            ["!b!c"],
-            ["aBBBBBBB"]
-        ]),
-        [ // Expecteds
-            "",
-            "Abc",
-            "!b!c",
-            "Abbbbbbb"
-        ]
-    ),
+
+const toTitleCaseTest = new Test(StringUtils.wordToTitleCase,true);
+toTitleCaseTest.runTests(
+    [
+        {input:[""],expected:""},
+        {input:["abc"],expected:"Abc"},
+        {input:["!b!c"],expected:"!b!c"},
+        {input:["aBBBBBBB"],expected:"Abbbbbbb"}
+    ],
     Test.isEqual
-)
+).then((v)=>{
+    carrousel.add(toTitleCaseTest.displayLastCases())
+})
